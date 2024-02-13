0df6742b1357ab7403ef8d11d6508736dc4de1de
Sunny <wntjs0305@gmail.com>
2024-02-13 18:59:15 +0900

    Animation)Rotating_Playing_Card

---
+++ Animation/Rotating_Playing_Card/Rotating_Playing_Card/Rotating_Playing_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rotating_Playing_CardApp.swift
+//  Rotating_Playing_Card
+//
+//  Created by JuSun Kang on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Rotating_Playing_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            P278_RotatingPlayingCard()
+        }
+    }
+}
