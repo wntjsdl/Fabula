5a78fa652f402a3aa35b6061c49c5123a933aaaa
Sunny <wntjs0305@gmail.com>
2024-02-21 13:13:21 +0900

    Animation_Raining

---
+++ Animation/Raining/Raining/RainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainingApp.swift
+//  Raining
+//
+//  Created by JuSun Kang on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct RainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            P275_Raining()
+        }
+    }
+}
