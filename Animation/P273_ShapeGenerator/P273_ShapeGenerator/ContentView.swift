7d9977bf36b7ddd1921709dadc3c82a4fa5ba30a
Sunny <wntjs0305@gmail.com>
2024-02-29 17:29:09 +0900

    P273_ShapeGenerator

---
+++ Animation/P273_ShapeGenerator/P273_ShapeGenerator/ContentView.swift
@@ -0,0 +1,189 @@
+//
+//  ContentView.swift
+//  P273_ShapeGenerator
+//
+//  Created by JuSun Kang on 2/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    public init() {}
+    public var body: some View {
+        SolarSystem()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+fileprivate struct SolarSystem: View {
+    @State private var shapeViewItems: [SolarSystemItem] = []
+    
+    var body: some View {
+        ZStack {
+            ForEach(shapeViewItems) { item in
+                item.view
+            }
+            
+            VStack(spacing: 10) {
+                Button("Add Shape") {
+                    addSolarSystemItem()
+                }
+                
+                Button("Remove Shape") {
+                    removeSolarSystemItem()
+                }
+            }
+            .buttonStyle(.bordered)
+        }
+    }
+    
+    private func addSolarSystemItem() {
+        shapeViewItems.append(.init(view: SolarSystemShape()))
+    }
+    
+    private func removeSolarSystemItem() {
+        if shapeViewItems.isEmpty { return }
+        shapeViewItems.remove(at: .random(in: 0..<shapeViewItems.count))
+    }
+}
+
+fileprivate struct SolarSystemItem: Identifiable {
+    var id = UUID()
+    var view: SolarSystemShape
+}
+
+fileprivate struct SolarSystemShape: View {
+    @State private var angle = Double.random(in: 0..<360)
+    @State private var a = Double.random(in: 0.25...0.9)
+    @State private var b = Double.random(in: 0.25...0.9)
+    
+    var body: some View {
+        GeometryReader { proxy in
+            ShapeView()
+                .modifier(EllipseAnimation(angle: angle, proxy: proxy, a: a, b: b))
+                .onAppear {
+                    withAnimation(.linear(duration: 3).repeatForever(autoreverses: false)) {
+                        angle += 360
+                    }
+                }
+        }
+    }
+}
+
+fileprivate struct EllipseAnimation: Animatable, ViewModifier {
+    var angle: Double
+    var proxy: GeometryProxy
+    var a: Double
+    var b: Double
+    
+    var animatableData: Double {
+        get { angle }
+        set { angle = newValue.truncatingRemainder(dividingBy: 360) }
+    }
+    
+    private var xOffset: Double {
+        (a / 2) * cos(2 * (angle / 360) * .pi) + 0.5
+    }
+    
+    private var yOffset: Double {
+        (b / 2) * sin(2 * (angle / 360) * .pi) + 0.5
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .offset(x: proxy.size.width * xOffset,
+                    y: proxy.size.height * yOffset)
+    }
+}
+
+fileprivate struct ShapeView: View {
+    var body: some View {
+        RandomShape()
+            .fill(Color(uiColor: getRandomColor()))
+            .frame(width: 25, height: 25)
+    }
+}
+
+fileprivate func getRandomColor() -> UIColor{
+    let randomRed = CGFloat.random(in: 0...1)
+    let randomGreen = CGFloat.random(in: 0...1)
+    let randomBlue = CGFloat.random(in: 0...1)
+    return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+}
+
+fileprivate struct RandomShape: Shape {
+    private var theShape: (any Shape)?
+    
+    private lazy var rectangle: Rectangle = { Rectangle() }()
+    private lazy var circle: Circle = { Circle() }()
+    private lazy var capsule: Capsule = { Capsule(style: .circular) }()
+    private lazy var star5: Star = { Star(count: 5, innerRatio: 1) }()
+    private lazy var star10: Star = { Star(count: 10, innerRatio: 1) }()
+    private lazy var ellipse = { Ellipse() }()
+    
+    init() {
+        switch Int.random(in: 0..<6) {
+        case 0: theShape = rectangle
+        case 1: theShape = circle
+        case 2: theShape = capsule
+        case 3: theShape = star5
+        case 4: theShape = star10
+        default: theShape = ellipse
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        theShape!.path(in: rect)
+    }
+}
+
+fileprivate struct Star: Shape {
+    var count: CGFloat
+    var innerRatio: CGFloat
+    
+    var animatableData: AnimatablePair<CGFloat, CGFloat> {
+        get { AnimatableData(count, innerRatio) }
+        set {
+            count = newValue.first
+            innerRatio = newValue.second
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        let center = CGPoint(x: rect.width * 0.5, y: rect.height * 0.5)
+        let pointAngle = .pi / count
+        
+        let innerPoint = CGPoint(x: center.x * innerRatio * 0.5, y: center.y * innerRatio * 0.5)
+        let totalPoints = Int(count * 2.0)
+        
+        var currentAngle = CGFloat.pi * -0.5
+        var currentBottom: CGFloat = 0
+        
+        var path = Path()
+        path.move(to: CGPoint(x: center.x * cos(currentAngle), y: center.y * sin(currentAngle)))
+        
+        let correction = count != round(count) ? 1 : 0
+        for corner in 0..<totalPoints + correction {
+            var bottom: CGFloat = 0
+            let sin = sin(currentAngle)
+            let cos = cos(currentAngle)
+            if (corner % 2) == 0 {
+                bottom = center.y * sin
+                path.addLine(to: CGPoint(x: center.x * cos, y: bottom))
+            } else {
+                bottom = innerPoint.y * sin
+                path.addLine(to: CGPoint(x: innerPoint.x * cos, y: bottom))
+            }
+            currentBottom = max(bottom, currentBottom)
+            currentAngle += pointAngle
+        }
+        
+        let transform = CGAffineTransform(translationX: center.x, y: center.y + ((rect.height * 0.5 - currentBottom) * 0.5))
+        return path.applying(transform)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Animation/P273_ShapeGenerator/P273_ShapeGenerator/P273_ShapeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  P273_ShapeGeneratorApp.swift
+//  P273_ShapeGenerator
+//
+//  Created by JuSun Kang on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct P273_ShapeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
